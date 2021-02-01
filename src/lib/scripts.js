71f077d5657b793aa4e8a375ae0164056b94a6ea
Jinu Kurian <jinu@whiterabbit.group>
2021-02-01 13:54:01 +0530

    removed locomotive scroll for the time being

--- src/lib/scripts.js
+++ src/lib/scripts.js
@@ -453,7 +453,7 @@
 }(jQuery));
 
 
-const scroller = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true
-})
+// const scroller = new LocomotiveScroll({
+//     el: document.querySelector('[data-scroll-container]'),
+//     smooth: true
+// })
